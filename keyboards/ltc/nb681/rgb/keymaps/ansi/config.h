0fa033f5eb48e66f2934a5498a569f291a946807
jonathan.clemons <clemons.jonathan@gmail.com>
2021-11-24 21:14:00 -0600

    [LTC NB681] Remove via config and update vendor & product id

--- keyboards/ltc/nb681/rgb/keymaps/ansi/config.h
+++ keyboards/ltc/nb681/rgb/keymaps/ansi/config.h
@@ -21,8 +21,8 @@
 #include "config_led.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0x0C45
-#define PRODUCT_ID      0x5004
+#define VENDOR_ID       0x4C54
+#define PRODUCT_ID      0x0681
 #define DEVICE_VER      0x0001
 
 #define MANUFACTURER    LTC
