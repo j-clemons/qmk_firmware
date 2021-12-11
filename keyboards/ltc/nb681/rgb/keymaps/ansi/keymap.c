e90add7b8993b5b38377e62170fa07d1f4b357ad
jonathan.clemons <clemons.jonathan@gmail.com>
2021-12-11 09:16:03 -0600

    add git shortcuts

--- keyboards/ltc/nb681/rgb/keymaps/ansi/keymap.c
+++ keyboards/ltc/nb681/rgb/keymaps/ansi/keymap.c
@@ -16,6 +16,42 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include QMK_KEYBOARD_H
 
+enum custom_keycodes {
+    GITB = SAFE_RANGE,
+    ATM,
+    GITSS,
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+    case GITB:
+        if (record->event.pressed) {
+          // when keycode is pressed
+          SEND_STRING("git branch"SS_TAP(X_ENT));
+        } else {
+          // when keycode is released
+        }
+        break;
+
+    case ATM:
+        if (record->event.pressed) {
+          SEND_STRING("atom ."SS_TAP(X_ENT));
+        } else {
+
+        }
+        break;
+
+    case GITSS:
+        if (record->event.pressed) {
+          SEND_STRING("git stash save ");
+        } else {
+
+        }
+        break;
+    }
+    return true;
+};
+
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
@@ -36,9 +72,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                   {   KC_LCTL,   KC_LGUI,   KC_LALT,   KC_NO,   KC_NO,    KC_NO,   KC_SPC,   KC_NO,   KC_NO,   KC_NO,     KC_RALT,   MO(_FN2),  KC_RCTRL,  KC_LEFT,   KC_DOWN,   KC_RGHT   }
                 },
       [_FN1] = {  {   KC_GESC,   KC_NO,     KC_NO,     KC_NO,   KC_NO,    KC_NO,   KC_NO,    KC_NO,   KC_NO,   KC_NO,     KC_MUTE,   KC_VOLD,   KC_VOLU,   KC_TRNS,   KC_NO,     KC_NO    },
-                  {   KC_NO,     KC_NO,     KC_NO,     KC_NO,   KC_NO,    KC_NO,   KC_NO,    KC_NO,   KC_NO,   KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_NO  },
-                  {   KC_NO,     KC_NO,     KC_NO,     KC_NO,   KC_NO,    KC_NO,   KC_LEFT,  KC_UP,   KC_DOWN, KC_RGHT,   KC_NO,     KC_NO,     KC_NO,     KC_TRNS,   KC_NO,     KC_NO    },
-                  {   KC_TRNS,   KC_NO,     KC_NO,     KC_NO,   C(KC_C),  C(KC_V), KC_NO,    KC_NO,   KC_NO,   KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_TRNS,   KC_NO  },
+                  {   KC_NO,     KC_NO,     KC_NO,     KC_NO,   KC_NO,    KC_NO,   KC_NO,    KC_NO,   KC_UP,   KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_NO  },
+                  {   KC_NO,     ATM,       GITSS,     KC_NO,   KC_NO,    KC_NO,   KC_NO,    KC_LEFT, KC_DOWN, KC_RGHT,   KC_NO,     KC_NO,     KC_NO,     KC_TRNS,   KC_NO,     KC_NO    },
+                  {   KC_TRNS,   KC_NO,     KC_NO,     KC_NO,   KC_NO,    KC_NO,   GITB,     KC_NO,   KC_NO,   KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_TRNS,   KC_NO  },
                   {   KC_TRNS,   KC_NO,     KC_NO,     KC_NO,   KC_NO,    KC_NO,   RESET,    KC_NO,   KC_NO,   KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_TRNS,   KC_TRNS,   KC_TRNS    }
                },
       [_FN2] = {  {   KC_GRV,    KC_F1,     KC_F2,     KC_F3,   KC_F4,    KC_F5,   KC_F6,    KC_F7,   KC_F8,   KC_F9,     KC_F10,    KC_F11,    KC_F12,    KC_NO,     KC_PSCR,   KC_HOME    },
