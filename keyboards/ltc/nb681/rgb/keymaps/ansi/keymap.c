e7433a16b3d685ebc24034171103b6f694e70a5f
jonathan.clemons <clemons.jonathan@gmail.com>
2022-01-18 11:52:29 -0600

    Add mouse clicks and underline

--- keyboards/ltc/nb681/rgb/keymaps/ansi/keymap.c
+++ keyboards/ltc/nb681/rgb/keymaps/ansi/keymap.c
@@ -72,10 +72,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                   {   KC_LCTL,   KC_LGUI,   KC_LALT,   KC_NO,   KC_NO,    KC_NO,   KC_SPC,   KC_NO,   KC_NO,   KC_NO,     KC_RALT,   MO(_FN2),  KC_RCTRL,  KC_LEFT,   KC_DOWN,   KC_RGHT   }
                 },
       [_FN1] = {  {   KC_GESC,   KC_NO,     KC_NO,     KC_NO,   KC_NO,    KC_NO,   KC_NO,    KC_NO,   KC_NO,   KC_NO,     KC_MUTE,   KC_VOLD,   KC_VOLU,   KC_TRNS,   KC_NO,     KC_NO    },
-                  {   KC_NO,     KC_NO,     KC_NO,     KC_NO,   KC_NO,    KC_NO,   KC_NO,    KC_NO,   KC_UP,   KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_NO  },
+                  {   KC_NO,     KC_NO,     KC_NO,     KC_NO,   KC_NO,    KC_NO,   KC_NO,LSFT(KC_MINS),KC_UP,  KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_NO  },
                   {   KC_NO,     ATM,       GITSS,     KC_NO,   KC_NO,    KC_NO,   KC_NO,    KC_LEFT, KC_DOWN, KC_RGHT,   KC_NO,     KC_NO,     KC_NO,     KC_TRNS,   KC_NO,     KC_NO    },
-                  {   KC_TRNS,   KC_NO,     KC_NO,     KC_NO,   KC_NO,    KC_NO,   GITB,     KC_NO,   KC_NO,   KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_TRNS,   KC_NO  },
-                  {   KC_TRNS,   KC_NO,     KC_NO,     KC_NO,   KC_NO,    KC_NO,   RESET,    KC_NO,   KC_NO,   KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_TRNS,   KC_TRNS,   KC_TRNS    }
+                  {   KC_TRNS,   KC_NO,     KC_NO,     KC_NO,   KC_NO,    KC_NO,   GITB,     KC_BTN1, KC_BTN2, KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_TRNS,   KC_NO  },
+                  {   KC_TRNS,   KC_NO,     KC_NO,     KC_NO,   KC_NO,    KC_NO,   KC_TRNS,  KC_NO,   KC_NO,   KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_TRNS,   KC_TRNS,   KC_TRNS    }
                },
       [_FN2] = {  {   KC_GRV,    KC_F1,     KC_F2,     KC_F3,   KC_F4,    KC_F5,   KC_F6,    KC_F7,   KC_F8,   KC_F9,     KC_F10,    KC_F11,    KC_F12,    KC_NO,     KC_PSCR,   KC_HOME    },
                   {   KC_NO,     KC_NO,     KC_UP,     KC_NO,   KC_NO,    KC_NO,   KC_NO,    KC_NO,   KC_NO,   KC_NO,     KC_NO,     KC_NO,     KC_NO,     RGB_MOD,   KC_PAUS,   KC_END  },
